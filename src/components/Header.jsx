8bf493c1ecad7f4e42a27b08bb598c6b34662255
LVSSandeepKumar <142000993+LVSSandeepKumar@users.noreply.github.com>
2023-11-04 18:46:43 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PublishedWithChangesIcon from "@mui/icons-material/PublishedWithChanges";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <PublishedWithChangesIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
